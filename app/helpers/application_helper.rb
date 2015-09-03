54e51fbfbf9285c2142f4a46f4b8bbc191e92fc7
Andrew Klotz <andrew.klotz@hotmail.com>
2015-09-03 17:00:04 -0400

    add bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+	    base_title = "HeroQueue, enter solo win as a team"
+	    if page_title.empty?
+			base_title
+	    else
+			page_title + " | " + base_title
+	    end
+	end	
 end
