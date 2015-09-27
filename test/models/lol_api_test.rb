ed56ffb45d2e0052b8b959346582eb2d8fc0e969
Andrew Klotz <andrew.klotz@hotmail.com>
2015-09-27 19:52:02 -0400

    tesy paypal openSSL encypt

---
+++ test/models/lol_api_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class LolApiTest < ActionDispatch::IntegrationTest
+	def setup
+		@ticket = tickets(:openSSL)
+	end
+
+	test "generates openSSL ticket link" do
+		ssl_value = PaypalApi.paypal_encrypted(@ticket, root_url, root_url)
+		assert_equal 2728, ssl_value.length
+	end
+end
