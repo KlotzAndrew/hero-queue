813bbe09b8b3459a2c889301ecd3ecd469ed05d3
Andrew Klotz <andrew.klotz@hotmail.com>
2015-09-28 03:19:48 -0400

    tests for teambalancer

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -18,6 +18,31 @@ class Tournament < ActiveRecord::Base
 		tickets.includes(:summoner, :duo).paid.duo_tickets.map {|x| [x.summoner, x.duo]}
 	end
 
+	def build_teams
+		return false if teams_closed? == true
+		return false if missing_elos? == true
+		teambuilder
+	end
+
+	def team_stats
+		elo_array = []
+		teams.each do |team|
+			elo_array << team.summoners.map {|x| x.elo }
+		end
+		team_mean = elo_array.flatten.sum/teams.count
+		Rails.logger.info "team_mean: #{team_mean}"
+		team_sums_sq = []
+		elo_array.each do |x|
+			Rails.logger.info "elo_array.each: #{x}"
+			team_sums_sq << (team_mean - x.sum)**2
+		end
+		Rails.logger.info "team_sums_sq: #{team_sums_sq}"
+		return {
+			team_std: (team_sums_sq.sum/(teams.count - 1))**0.5,
+			team_mean: team_mean/5
+		}
+	end
+
 	#plan to remove this when 4+ new tournaments
 	def self.legacy
 		array = []
@@ -43,4 +68,63 @@ class Tournament < ActiveRecord::Base
 			facebook_url: "https://www.facebook.com/events/836624339681535")
 		return array
 	end	
+
+	private
+
+	def teams_closed?
+		return true if seats_left > 0
+		return true if teams.count == total_teams
+	end
+
+	def missing_elos?
+		check_elo = Proc.new {|x| return true if x.elo.nil?}
+		all_solos.each(&check_elo)
+		all_duos.flatten.each(&check_elo)
+	end
+
+	def teambuilder
+		cand_teams = []
+		cand_std = 2000
+		st = Time.now.to_i
+
+		#populate teams
+		p_solos = all_solos
+		p_duos = all_duos
+		all_players = all_solos + all_duos.flatten
+		ids = all_players.map {|x| x.elo}
+		Rails.logger.info "MAIN! #{ids}"
+		Rails.logger.info "TCIKETS! #{tickets.where(status: "Completed").map {|x| [x.summoner_id, x.duo_id]}}"
+		Rails.logger.info "duplicates! #{ids.select{|item| ids.count(item) > 1}.uniq}"
+
+		temp_teams = []
+		total_teams.times do |x|
+			temp_teams << []
+		end
+
+		p_duos.each do |duo|
+			team_num = rand(0..total_teams-1)
+			while temp_teams[team_num].flatten.count >= 4
+				team_num = rand(0..total_teams-1)
+			end
+
+			temp_teams[team_num] << duo
+		end
+
+		p_solos.each do |solo|
+			team_num = rand(0..total_teams-1)
+			while temp_teams[team_num].flatten.count >= 5
+				team_num = rand(0..total_teams-1)
+			end
+			temp_teams[team_num] << solo
+		end
+
+		temp_teams.each do |team_array|
+			Rails.logger.info "TEAM_ARRAY: #{team_array}"
+			team = Team.create
+			team_array.flatten.each do |x|
+				team.summoners << x
+			end
+			teams << team
+		end
+	end
 end
--- test/models/tournament_test.rb
+++ test/models/tournament_test.rb
@@ -3,8 +3,10 @@ require 'test_helper'
 class TournamentTest < ActiveSupport::TestCase
 	def setup
 		@tournament = tournaments(:tournament_sold)
+		@tournament_unsold = tournaments(:tournament_unsold)
 	end
 
+	#instance scopes
 	test "player_count returns corrent number" do
 		assert_equal 40, @tournament.player_count
 	end
@@ -20,5 +22,71 @@ class TournamentTest < ActiveSupport::TestCase
 	test "all_duos returns corrent number" do
 		assert_equal 12, @tournament.all_duos.count
 		assert_equal 24, @tournament.all_duos.flatten.count
+	end
+
+	#teambuilder
+	test "needs full roster to build teams" do
+		@tournament_unsold.build_teams
+		assert_equal 0, @tournament_unsold.reload.teams.count
+	end
+
+	test "does not build teams if teams full" do
+		@tournament.total_teams.times do |x|
+			@tournament.teams << Team.create
+		end
+		@tournament = @tournament.reload
+		assert_equal @tournament.total_teams, @tournament.teams.count
+		assert_equal false, @tournament.build_teams
+	end
+
+	test "required summoner elo to build teams" do
+		assert_equal 0, @tournament.reload.teams.count
+		assert_equal false, @tournament.build_teams
+	end
+
+	test "builds teams correctly" do
+		all_players = @tournament.all_solos + @tournament.all_duos.flatten
+		i = 0
+		all_players.each do |x|
+			if i <= 5 
+				x.update(elo: 1000)
+			elsif i < 10 
+				x.update(elo: 1500)
+			elsif i < 20 
+				x.update(elo: 2000)
+			elsif i < 30 
+				x.update(elo: 2500)
+			elsif i < 35 
+				x.update(elo: 3000)
+			else 
+				x.update(elo: 3500) 
+			end
+			i+=1
+		end
+		@tournament.build_teams
+		@tournament = @tournament.reload
+
+		#all teams are created
+		assert_equal @tournament.total_teams, @tournament.teams.count
+		#each team is 5 players
+		@tournament.teams.each do |x|
+			assert_equal 5, x.summoners.count
+		end
+		#each player is on a team (once)
+		@tournament.all_solos.each do |x|
+			assert_equal 1, x.teams.count
+		end
+		@tournament.all_duos.flatten.each do |x|
+			assert_equal 1, x.teams.count
+		end
+		#duos are still together
+		@tournament.all_duos.each do |x,y|
+			assert_equal x.teams.first, y.teams.first
+		end
+		#returns correct stats
+		assert_equal 2237, @tournament.team_stats[:team_mean]
+		#is balanced
+		assert_includes 0..100, @tournament.team_stats[:team_std]
+		#range maximum?
 	end
 end
