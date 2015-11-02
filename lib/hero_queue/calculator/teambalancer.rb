69a6384499ed47cc733dedafa194c6e0da18344b
Andrew Klotz <andrew.klotz@hotmail.com>
2015-11-02 02:17:12 -0500

    reworked teambalancer with mixpot

--- lib/hero_queue/calculator/teambalancer.rb
+++ lib/hero_queue/calculator/teambalancer.rb
@@ -6,6 +6,8 @@ module Calculator
 
 		Rails.env == "test" ? TIME_TOTAL = 1 : TIME_TOTAL = 20
 		Rails.env == "test" ? TIME_MIXER = 1 : TIME_MIXER = 1
+		TARGET_STD = 100
+		TEAM_SIZE = 5
 
 		def initialize(solos, duos)
 			@solos = parse_solos(solos)
@@ -14,37 +16,14 @@ module Calculator
 		end
 
 		def teambalance(options = {})
-			teams_hash = seed_teams
-			teams_hash = optimize_teams(teams_hash)
-			return teams_hash[:teams]
-		end
-
-		def seed
-			teams_hash = seed_teams
-		end
-
-		def isolate_two_teams
-			teams_hash = seed_teams
-			teams_hash = extract_random_teams(teams_hash)
-		end
-
-		def shuffle_two_teams
-			teams_hash = seed_teams
-			teams_hash = extract_random_teams(teams_hash)
-			teams_hash = suffle_extracted_teams(teams_hash)
-		end
-
-		def merge_back_teams
-			teams_hash = seed_teams
-			teams_hash = extract_random_teams(teams_hash)
-			teams_hash = suffle_extracted_teams(teams_hash)
-			teams_hash = merge_suffle_to_main(teams_hash)
+			teams_hash = seed_teams(@total_teams, @solos, @duos)
+			teams_hash = optimize_teams(teams_hash)[:teams]
 		end
 
 		def optimize_teams(teams_hash)
 			st = Time.now.to_i
-			candidate_std = 2000
-			while (candidate_std >= 100) && (Time.now.to_i - st) < TIME_TOTAL
+			candidate_std = teams_hash[:elo_sums].standard_deviation
+			while (candidate_std >= TARGET_STD) && (Time.now.to_i - st) < TIME_TOTAL
 				teams_hash = shuffle_teams(teams_hash)
 				candidate_std = teams_hash[:elo_sums].standard_deviation
 			end
@@ -52,70 +31,47 @@ module Calculator
 		end
 
 		def shuffle_teams(teams_hash)
-			teams_hash = extract_random_teams(teams_hash)
-			teams_hash = suffle_extracted_teams(teams_hash)
-			teams_hash = merge_suffle_to_main(teams_hash)
+			random_teams_index = pick_random_teams
+			teams_new = calc_new_teams(teams_hash, random_teams_index)
+			teams_hash = remove_teams_old(teams_hash, random_teams_index)
+			teams_hash = merge_teams_to_main(teams_hash, teams_new, random_teams_index)
 		end
 
-		def merge_suffle_to_main(teams_hash)
-			teams_hash[:teams_mix].each {|x| teams_hash[:teams] << x}
-			teams_hash[:elo_sums_mix].each {|x| teams_hash[:elo_sums] << x}
-			return teams_hash
-		end
-
-		def suffle_extracted_teams(teams_hash)
+		def calc_new_teams(teams_hash, random_teams_index)
+			# mix = random_teams_index.each_with_object(Array.new) {|i, a| a += teams_hash[:teams][i]}
+			mix = Array.new
+			random_teams_index.each {|x| mix = mix + teams_hash[:teams][x]}
+			candidate_diff = baseline_diff = (teams_hash[:elo_sums].max - teams_hash[:elo_sums].min)
 			st2 = Time.now.to_i
-			while teams_hash[:teams_diff] <= (teams_hash[:elo_sums_mix].max - teams_hash[:elo_sums_mix].min) && (Time.now.to_i - st2) < TIME_MIXER
-				teams_hash = shuffle_mixpot_teams(teams_hash)
+			while candidate_diff >= baseline_diff && (Time.now.to_i - st2) < TIME_MIXER
+				teams_new = itterate_teams(mix)
+				candidate_diff = (teams_new[:elo_sums].max - teams_new[:elo_sums].min)
 			end
-			return teams_hash
+			return teams_new
 		end
 
-		def extract_random_teams(teams_hash)
-			teams_ab = pick_random_teams
-			#clear elo sums
-			0.upto(teams_hash[:elo_sums].count-1) {|x| teams_hash[:elo_sums][x] = 0}
-			#move players
-			teams_hash = move_solo_duo_to_mix(teams_hash, 2, teams_ab, "teams")
-			teams_hash = move_solo_duo_to_mix(teams_hash, 1, teams_ab, "teams")
-			delete_at_multi(teams_hash[:teams], teams_ab)
-			delete_at_multi(teams_hash[:elo_sums], teams_ab)
+		def remove_teams_old(teams_hash, random_teams_index)
+			delete_at_multi(teams_hash[:teams], random_teams_index)
+			delete_at_multi(teams_hash[:elo_sums], random_teams_index)
+			teams_hash
+		end
+
+		def itterate_teams(mix)
+			total_teams = mix.flatten.count/TEAM_SIZE
+			solos_duos = mix.partition {|x| x.count > 1}
+			teams_new = seed_teams(total_teams, solos_duos[1], solos_duos[0])
+		end
+
+		def merge_teams_to_main(teams_hash, teams_new, random_teams_index)
+			teams_new[:teams].each {|x| teams_hash[:teams] << x}
+			teams_new[:elo_sums].each {|x| teams_hash[:elo_sums] << x}
 			return teams_hash
 		end
 
 		def pick_random_teams
-			team_a = rand(0..@total_teams-1)
-			team_b = rand(0..@total_teams-1)
-			while team_a == team_b
-				team_b = rand(0..@total_teams-1)
-			end
-			return [team_a, team_b]
-		end
-
-		def shuffle_mixpot_teams(teams_hash)
-			mix = teams_hash[:teams_mix][0] + teams_hash[:teams_mix][1]
-			0.upto(teams_hash[:elo_sums_mix].count-1) {|x| teams_hash[:elo_sums_mix][x] = 0}
-			0.upto(teams_hash[:teams_mix].count-1) {|x| teams_hash[:teams_mix][x] = []}	
-			teams_hash = move_solo_duo_to_mix(teams_hash, 2, [0,1], "teams_mix", mix)
-			teams_hash = move_solo_duo_to_mix(teams_hash, 1, [0,1], "teams_mix", mix)
-			teams_hash[:teams_diff] = (teams_hash[:elo_sums_mix].max - teams_hash[:elo_sums_mix].min)
-			return teams_hash
-		end
-
-		def move_solo_duo_to_mix(teams_hash, group_size, teams_ab, base_duck, mix = nil)
-			mix ||= teams_hash[base_duck.to_sym][teams_ab[0]] + teams_hash[base_duck.to_sym][teams_ab[1]]
-			mix.each do |player|
-				teams_hash = move_play_to_mix(teams_hash, player) if player.count == group_size
-			end
-			teams_hash[:teams_diff] = teams_hash[:elo_sums_mix].max - teams_hash[:elo_sums_mix].min
-			return teams_hash
-		end
-
-		def move_play_to_mix(teams_hash, player)
-			random_team = random_team_with_space(teams_hash[:teams_mix], player.count)
-			teams_hash[:teams_mix][random_team] << player
-			player.each {|x| teams_hash[:elo_sums_mix][random_team]+= x[:elo] }
-			return teams_hash
+			a = b = rand(0..@total_teams-1)
+			b = rand(0..@total_teams-1) while a == b
+			return [a, b]
 		end
 
 		def random_team_with_space(teams_ary, group_size)
@@ -127,24 +83,24 @@ module Calculator
 			return team_number
 		end	
 
-		def seed_teams
-			teams_hash = {
-				teams: Array.new(@total_teams) {[]},
-				elo_sums: Array.new(@total_teams) {0},
-				teams_mix: Array.new(2){[]},
-				elo_sums_mix: Array.new(2){0}
+		def seed_teams(total_teams, solos, duos)
+			new_teams_hash = {
+				teams: Array.new(total_teams) {[]},
+				elo_sums: Array.new(total_teams) {0},
+				# teams_mix: Array.new(2){[]},
+				# elo_sums_mix: Array.new(2){0}
 			}
-			@duos.each do |player|
-				random_team = random_team_with_space(teams_hash[:teams], player.count)
-				teams_hash[:teams][random_team] << player
-				teams_hash[:teams][random_team][0].each {|h| teams_hash[:elo_sums][random_team] += h[:elo]}
+			duos.each do |player|
+				random_team = random_team_with_space(new_teams_hash[:teams], player.count)
+				new_teams_hash[:teams][random_team] << player
+				new_teams_hash[:teams][random_team][0].each {|h| new_teams_hash[:elo_sums][random_team] += h[:elo]}
 			end
-			@solos.each do |player|
-				random_team = random_team_with_space(teams_hash[:teams], player.count)
-				teams_hash[:teams][random_team] << player
-				teams_hash[:teams][random_team][0].each {|h| teams_hash[:elo_sums][random_team] += h[:elo]}
+			solos.each do |player|
+				random_team = random_team_with_space(new_teams_hash[:teams], player.count)
+				new_teams_hash[:teams][random_team] << player
+				new_teams_hash[:teams][random_team][0].each {|h| new_teams_hash[:elo_sums][random_team] += h[:elo]}
 			end
-			return teams_hash
+			return new_teams_hash
 		end
 
 		def tally_players(solos, duos)
