f5eb9c1d6dc5890990b16462933a0605b4ed9044
Andrew Klotz <andrew.klotz@hotmail.com>
2015-11-22 01:20:32 -0500

    added js for toggling series menus

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,12 +16,48 @@
 //= require turbolinks
 //= require_tree .
 
-// $(document).on('page:load', function() {
-//   console.log("working? i + s")
-// })
+$(document).on('page:load', function() {
+  series_info_toggle();
+})
 
 $(document).ready(function() {
   series_info_toggle();
 })
 
-var series_info_toggle
+var series_info_toggle = function(){
+	$('#crumb_1').on("click", function(){
+		if ($('#bread_1').hasClass("hide") == true) {
+			$('#bread_1').toggleClass("hide")
+		}
+		if ($('#bread_2').hasClass("hide") == false) {
+			$('#bread_2').toggleClass("hide")
+		}
+		if ($('#bread_3').hasClass("hide") == false) {
+			$('#bread_3').toggleClass("hide")
+		}
+	})
+
+	$('#crumb_2').on("click", function(){
+		if ($('#bread_1').hasClass("hide") == false) {
+			$('#bread_1').toggleClass("hide")
+		}
+		if ($('#bread_2').hasClass("hide") == true) {
+			$('#bread_2').toggleClass("hide")
+		}
+		if ($('#bread_3').hasClass("hide") == false) {
+			$('#bread_3').toggleClass("hide")
+		}
+	})
+
+	$('#crumb_3').on("click", function(){
+		if ($('#bread_1').hasClass("hide") == false) {
+			$('#bread_1').toggleClass("hide")
+		}
+		if ($('#bread_2').hasClass("hide") == false) {
+			$('#bread_2').toggleClass("hide")
+		}
+		if ($('#bread_3').hasClass("hide") == true) {
+			$('#bread_3').toggleClass("hide")
+		}
+	})
+}
